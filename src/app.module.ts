962317fc6aa62df72fda19f83ba0b1f32d614e83
matheus mozart <mmsnborges@gmail.com>
2025-01-01 23:49:28 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1/fusion', {
+    serverSelectionTimeoutMS: 50000,
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
