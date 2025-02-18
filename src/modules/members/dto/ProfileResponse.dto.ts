849b9672f697c63414de93471ea9ce9b02200171
g42puts <gilmar.neo@gmail.com>
2025-02-18 08:08:18 -0300

    chore: removed unused

--- src/modules/members/dto/ProfileResponse.dto.ts
+++
@@ -1,8 +0,0 @@
-export class ProfissionalProfileResponse {
-  platform: string;
-  url: string;
-
-  constructor(platform: string, url: string) {
-    (this.platform = platform), (this.url = url);
-  }
-}
